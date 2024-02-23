3b0746c04b5fc4c167dba47b9f1772eed6b5c23b
000itsuki000 <itsuki.h@collabo-style.co.jp>
2024-02-23 15:03:30 +0900

    Initial Commit

---
+++ TableViewInCellDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewInCellDemo
+//
+//  Created by Itsuki on 2024/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
